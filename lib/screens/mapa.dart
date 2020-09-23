dc63569330c59784f606944db0e7c92a21536fd1
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-09-23 20:44:32 -0300

    esqueleto da classe Mapa

--- lib/screens/mapa.dart
+++ lib/screens/mapa.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shift_a2_mapas_conec/model/listPositions.dart';
+import 'package:flutter_shift_a2_mapas_conec/model/position.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Mapa extends StatelessWidget {
+class Mapa extends StatefulWidget {
+  @override
+  _MapaState createState() => _MapaState();
+}
+
+class _MapaState extends State<Mapa> {
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
+  Position position;
+  ListPositions listPositions;
+
+  CameraPosition _position;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
   }
+
 }
