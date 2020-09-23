f311bede5ec99fa95722b6d1b6d60d404c55d85e
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-09-23 19:58:49 -0300

    esqueleto da listagem de carros, já com a conectividade funcional

--- lib/screens/listCars.dart
+++ lib/screens/listCars.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shift_a2_mapas_conec/model/listPositions.dart';
+import 'package:flutter_shift_a2_mapas_conec/model/position.dart';
+import 'package:flutter_shift_a2_mapas_conec/service/endpoints.dart';
 
-class ListCars extends StatelessWidget {
+class ListCars extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _ListCarsState();
+
+}
+
+class _ListCarsState extends State<ListCars> {
+
+  ListPositions listPositions;
+
+  //initState faz parte do ciclo de vida do State
+  //e nesse ciclo de vida, ele é a primeira chamada
+  @override
+  void initState() {
+    super.initState();
+
+    getPositions().then((list) {
+      setState(() {
+        listPositions = list;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: listPositions == null ? LinearProgressIndicator() :
+      ListView.separated(
+          itemCount: listPositions.positions.length,
+          itemBuilder: (context, index) =>
+              buildListItem(listPositions.positions[index]),
+          separatorBuilder: (context, index) => Divider(
+            height: 1,
+          ),
+      ),
+    );
   }
+
+  Widget buildListItem(Position position){
+    return Text(position.condutor_nome); // ListItemVehicle(position);
+  }
+
 }
