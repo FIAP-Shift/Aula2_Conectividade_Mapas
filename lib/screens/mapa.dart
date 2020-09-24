a5631d0083e6afaf08d976b1b1b70d6b16198926
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-09-23 21:05:41 -0300

    tela do mapa finalizada

--- lib/screens/mapa.dart
+++ lib/screens/mapa.dart
@@ -22,9 +22,90 @@ class _MapaState extends State<Mapa> {
     super.initState();
   }
 
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    if (position != null) {
+      final marker = Marker(
+        markerId: MarkerId(position.veiculo_placa),
+        position: LatLng(position.lat, position.lng),
+      );
+      markers[marker.markerId] = marker;
+    } else {
+      List<LatLng> latLngs = List();
+      for (final position in listPositions.positions) {
+        LatLng latLng = LatLng(position.lat, position.lng);
+        latLngs.add(latLng);
+        final marker = Marker(
+          markerId: MarkerId(position.veiculo_placa),
+          position: latLng,
+          infoWindow: InfoWindow(
+            title: position.veiculo_placa,
+            snippet: position.condutor_nome,
+          ),
+        );
+        markers[marker.markerId] = marker;
+      }
+
+      LatLngBounds bounds = boundsFromLatLngList(latLngs);
+      controller.animateCamera(
+        CameraUpdate.newLatLngBounds(bounds, 45.0),
+      );
+    }
+
+    setState(() {});
+  }
+
+  //função que itera sobre as posições geográficas, construíndo o ponto mais a nordeste e sudoeste
+  LatLngBounds boundsFromLatLngList(List<LatLng> list) {
+    double x0, x1, y0, y1;
+    for (LatLng latLng in list) {
+      if (x0 == null) {
+        x0 = x1 = latLng.latitude;
+        y0 = y1 = latLng.longitude;
+      } else {
+        if (latLng.latitude > x1) x1 = latLng.latitude;
+        if (latLng.latitude < x0) x0 = latLng.latitude;
+        if (latLng.longitude > y1) y1 = latLng.longitude;
+        if (latLng.longitude < y0) y0 = latLng.longitude;
+      }
+    }
+    //nordeste
+    //sudoeste
+    return LatLngBounds(northeast: LatLng(x1, y1), southwest: LatLng(x0, y0));
+  }
+
   @override
   Widget build(BuildContext context) {
+    buildPositionVariables(context);
 
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _position,
+        onMapCreated: _onMapCreated,
+        markers: Set<Marker>.of(markers.values),
+      ),
+    );
+
+  }
+
+  void buildPositionVariables(BuildContext context){
+    if (ModalRoute.of(context).settings.arguments is Position){
+      position = ModalRoute.of(context).settings.arguments;
+    } else {
+      listPositions = ModalRoute.of(context).settings.arguments;
+    }
+
+    if (position != null) {
+      _position = CameraPosition(
+        target: LatLng(position.lat, position.lng),
+        zoom: 14.4746,
+      );
+    } else {
+      _position = CameraPosition(
+        target: LatLng(listPositions.positions[0].lat, listPositions.positions[0].lng),
+        zoom: 18.4746,
+      );
+    }
   }
 
 }
